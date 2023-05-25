77857a9861349fe56060d74e9b1ce2beaf42ca3a
codersuresh <sd807141@gmail.com>
2023-05-25 12:14:31 +0545

    style setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bg': "var(--bg)",
+        'darkBg': "var(--dark-bg)",
+        'text': "var(--text)",
+        'darkText': "var(--dark-text)",
+        'inputColor': "var(--input-color)",
+        'elem': "var(--elem)",
+        'darkElem': "var(--dark-elem)",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 }
-
