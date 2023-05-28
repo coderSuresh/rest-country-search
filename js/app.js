b1b9d162c07760f336ba720ed3e1419f6b4bdccb
codersuresh <sd807141@gmail.com>
2023-05-28 14:45:01 +0545

    refactor: separate renderDetails function

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ import { toggleDarkMode, setDarkMode, setLightMode } from "./dark-mode.js"
 import { getCountries } from "./render-countries.js"
 import { setFilterList } from "./set-filter-list.js"
 import { searchCountry } from "./search-country.js"
+import { renderCountryDetails } from "./render-details.js"
 
 const filterDropDown = document.querySelector(".drop-down")
 const uiMode = localStorage.getItem("uiMode") ? localStorage.getItem("uiMode") : ""
@@ -27,45 +28,4 @@ document.addEventListener("click", (e) => {
 getCountries()
 setFilterList()
 searchCountry()
-
-const renderCountryDetails = () => {
-    const detailsContainer = document.querySelector(".country--details")
-    const countryName = location.href.includes("?country=") ? location.href.split("?country=")[1].replace(/%20/g, " ")  : "" //replace %20 with space in country name globally (g) in the string
-
-    if (detailsContainer && countryName) {
-        fetch("../data.json")
-            .then((res) => res.json())
-            .then((data) => {
-                data = data.filter((country) => country.name.toLowerCase() === countryName)[0]
-                fetch("../template-country-details.html")
-                    .then((res) => res.text())
-                    .then((template) => {
-
-                        const languages = data.languages.map((language) => language.name).join(", ")
-                        const currencies = data.currencies.map((currency) => currency.name).join(", ")
-                        const borders = data.borders.map((border) => {
-                            return `
-                                    <p class="w-fit bg-elem px-4 rounded flex-grow text-center">${border}</p>
-                                `
-                        }).join("")
-
-                        const countryDetails = template
-                            .replace("{country}", data.name)
-                            .replace("{flag}", data.flags.svg)
-                            .replace("{native name}", data.nativeName)
-                            .replace("{population}", data.population)
-                            .replace("{region}", data.region)
-                            .replace("{sub region}", data.subregion)
-                            .replace("{capital}", data.capital)
-                            .replace("{tld}", data.topLevelDomain)
-                            .replace("{currencies}", currencies)
-                            .replace("{languages}", languages)
-                            .replace("{borders}", borders)
-
-                        detailsContainer.innerHTML = countryDetails
-                    })
-            })
-    }
-}
-
 renderCountryDetails()
